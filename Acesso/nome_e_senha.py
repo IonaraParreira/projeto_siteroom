12abefadb2610451479d4ecda75ca625c4e02076
Ionara <ionara.parreira@gmail.com>
2025-12-30 09:48:20 -0300

    atualização do formato

--- Acesso/nome_e_senha.py
+++ Acesso/nome_e_senha.py
@@ -1,18 +1,14 @@
 import getpass
 
 #1 Coletando os dados do usuário
-usuario = input("\nDigite o nome de usuário: ").strip() #Remove espaços em branco
-senha = getpass.getpass("""\n Digite a \n
-█▀▀ █▀▀ █▀▀▄ █░░█ █▀▀█ 
-▀▀█ █▀▀ █░░█ █▀▀█ █▄▄█ 
-▀▀▀ ▀▀▀ ▀░░▀ ▀░░▀ ▀░░▀:""")
+usuario = input("\nDigite o nome certo: ").strip() #Remove espaços em branco
+senha = getpass.getpass("""\n Coloque o código:""")
 
 #2 Verificando as credenciais
-if usuario.lower() == "admin" and senha == "12345":
-    print(f"\n Bem-vinda de volta,{usuario.title()}!")
-    print("Mais um membro no jogo!")
-elif usuario.lower() == "admin" and senha == "admin123":
-    print(f"\n Modo Adiministrador ativado.Olá,{usuario.upper()}!")
+if usuario.lower() == "zoye" and senha == "451":
+    print(f"\n Mais um membro no jogo!")
+    print("Isso está ficando cada vez mais interessante!")
+
 else:
     print("""\n 
 ▄▀█ █▀▀ █▀▀ █▀ █▀ █▀█   █▄░█ █▀▀ █▀▀ ▄▀█ █▀▄ █▀█
